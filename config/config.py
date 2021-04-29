fb1bedf456678e939f02ff4ff31ba09d2bd38962
justice ndou <mobiusndou@gmail.com>
2021-04-30 00:43:37 +0200

    updated config file

--- config/config.py
+++ config/config.py
@@ -1,6 +1,6 @@
 from decouple import config
-
+import os
 class Config:
-    EOD_HISTORICAL_DATA_API_KEY_ENV_VAR: str = config('eod_api_key')
-    EOD_HISTORICAL_DATA_API_KEY_DEFAULT: str = config('eod_api_key')
+    EOD_HISTORICAL_DATA_API_KEY_ENV_VAR: str = os.getenv('EOD_HISTORICAL_API_KEY') or config('EOD_HISTORICAL_API_KEY')
+    EOD_HISTORICAL_DATA_API_KEY_DEFAULT: str = os.getenv('EOD_HISTORICAL_API_KEY') or config('EOD_HISTORICAL_API_KEY')
     EOD_HISTORICAL_DATA_API_URL: str = "https://eodhistoricaldata.com/api"
